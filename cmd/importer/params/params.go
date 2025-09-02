448b5b21374323ff78d594cb0a8c328cc868eaae
Anjan Nath <kaludios@gmail.com>
2025-09-02 18:59:25 +0530

    cmd: add flags to replicate image to region during upload

--- cmd/importer/params/params.go
+++ cmd/importer/params/params.go
@@ -5,15 +5,17 @@ import (
 )
 
 const (
-	BackedURL         string = "backed-url"
-	BackedURLDesc     string = "backed for stack state. (local) file:///path/subpath (s3) s3://existing-bucket, (azure) azblob://existing-blobcontainer. See more https://www.pulumi.com/docs/iac/concepts/state-and-backends/#using-a-self-managed-backend"
-	Output            string = "output"
-	OutputDesc        string = "path to export information regarding the cloud image"
-	Debug             string = "debug"
-	DebugDesc         string = "Enable debug traces and set verbosity to max. Typically to get information to troubleshooting an issue."
-	DebugLevel        string = "debug-level"
-	DebugLevelDefault uint   = 3
-	DebugLevelDesc    string = "Set the level of verbosity on debug. You can set from minimum 1 to max 9."
+	BackedURL          string = "backed-url"
+	BackedURLDesc      string = "backed for stack state. (local) file:///path/subpath (s3) s3://existing-bucket, (azure) azblob://existing-blobcontainer. See more https://www.pulumi.com/docs/iac/concepts/state-and-backends/#using-a-self-managed-backend"
+	Output             string = "output"
+	OutputDesc         string = "path to export information regarding the cloud image"
+	Debug              string = "debug"
+	DebugDesc          string = "Enable debug traces and set verbosity to max. Typically to get information to troubleshooting an issue."
+	DebugLevel         string = "debug-level"
+	DebugLevelDefault  uint   = 3
+	DebugLevelDesc     string = "Set the level of verbosity on debug. You can set from minimum 1 to max 9."
+	ParamReplicate     string = "replicate"
+	ParamReplicateDesc string = "Provide a list of location to replicate or 'all' to replicate to all available locations"
 )
 
 func AddCommonFlags(fs *pflag.FlagSet) {
